2b11e724d4204f7435ac6c4c4fb973faa8c52b5f
Luke Plant <L.Plant.98@cantab.net>
2012-12-05 14:18:05 +0000

    Fixed virtualenv issue on Python 2.7

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ then
 fi
 
 echo "installing theory prerequisites and Python virtual environment to $SELF/env/"
-python go-pylons.py --no-site-packages "$SELF/env/" &&
+python2.6 go-pylons.py --no-site-packages "$SELF/env/" &&
 "$SELF/env/bin/easy_install" python-mpd
 
